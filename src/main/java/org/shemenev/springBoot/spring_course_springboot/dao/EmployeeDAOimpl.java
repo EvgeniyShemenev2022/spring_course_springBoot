f8e7584e1ce803ac039f6bb824096fd9b9683d9d
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-12-21 20:17:23 +0300

    методы переписаны без применения функционала Hibernate; использован пакет import jakarta.persistence.Query;

--- src/main/java/org/shemenev/springBoot/spring_course_springboot/dao/EmployeeDAOimpl.java
+++ src/main/java/org/shemenev/springBoot/spring_course_springboot/dao/EmployeeDAOimpl.java
@@ -2,8 +2,9 @@ package org.shemenev.springBoot.spring_course_springboot.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+//import org.hibernate.Session;
+//import org.hibernate.query.Query;
+import jakarta.persistence.Query;
 import org.shemenev.springBoot.spring_course_springboot.entity.Employee;
 import org.springframework.stereotype.Repository;
 
@@ -19,34 +20,49 @@ public class EmployeeDAOimpl implements EmployeeDAO{
     @Override
     public List<Employee> getAllEmployees() {
 
-        Session session = entityManager.unwrap(Session.class);
-        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+//        Session session = entityManager.unwrap(Session.class);
+//        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+//        List<Employee> allEmployees = query.getResultList();
+
+        Query query = entityManager.createQuery("from Employee");
         List<Employee> allEmployees = query.getResultList();
 
         return allEmployees;
     }
 
-//    @Override
-//    public void saveEmployee(Employee employee) {
-//
-//        Session session = factory.getCurrentSession();
+    @Override
+    public void saveEmployee(Employee employee) {
+
+//        Session session = entityManager.unwrap(Session.class);
 //        session.saveOrUpdate(employee);
-//    }
-//
-//    @Override
-//    public Employee getEmployee(int id) {
-//
-//        Session session = factory.getCurrentSession();
+
+            // для того, чтобы в респонсе получать адекватный id из базы
+            Employee newEmployee = entityManager.merge(employee);
+            employee.setId(newEmployee.getId());
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+
+//        Session session = entityManager.unwrap(Session.class);
 //        Employee employee = session.get(Employee.class, id);
-//
-//        return employee;
-//    }
-//
-//    @Override
-//    public void deleteEmployee(int id) {
-//        Session session = factory.getCurrentSession();
+
+        Employee employee = entityManager.find(Employee.class, id);
+
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+//        Session session = entityManager.unwrap(Session.class);
 //        Query<Employee> query = session.createQuery("delete from Employee where id =:employeeID");
 //        query.setParameter("employeeID", id);
 //        query.executeUpdate();
-//    }
+
+
+        Query query = entityManager.createQuery("delete from Employee where id =:employeeID");
+        query.setParameter("employeeID", id);
+        query.executeUpdate();
+
+    }
 }
